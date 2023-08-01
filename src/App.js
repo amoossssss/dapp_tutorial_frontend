f25283200ac2b5edcb5f0058f99f651d132245c1
AmosWu <amos70180@gmail.com>
2023-08-01 15:52:11 +0800

    [Feat](all) Init tutorial frontend project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import MessageBlock from "./components/MessageBlock";
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <MessageBlock/>
+    </div>
+  );
+}
+
+export default App;
